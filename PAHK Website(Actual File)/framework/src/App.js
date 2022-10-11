4197105c781936089e7cad5d2cde7fda74d70501
Andy Poon <andypoon923@gmail.com>
2022-10-12 00:13:23 +0800

    Cleaned up

--- PAHK Website(Actual File)/framework/src/App.js
+++ PAHK Website(Actual File)/framework/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import TodoList from "./TodoList";
 
 function App() {
+  const [todos, Todos] = useState(['todo1','todo2'])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+       <TodoList todos = {todos}/>
+       <input type= "text" />
+       <button>Add todos</button>
+       <button>clear complete</button>
+    </>
+  )
 }
 
 export default App;
