3c73827c1f69ecdf7eefd25ccfba746801ec32fc
Andy Poon <andypoon923@gmail.com>
2022-10-12 18:47:52 +0800

    nav bar skeleton added

--- PAHK Website(Actual File)/framework/src/index.js
+++ PAHK Website(Actual File)/framework/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'
 import Footer from './Footer';
 import Header from './Header';
 import Navibar from './Navibar';
@@ -7,7 +8,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
+    
+    <BrowserRouter>
     <Navibar/>
+    </BrowserRouter>
+    
     <Footer />
   </React.StrictMode>
 );
--- PAHK Website(Actual File)/framework/src/navibar.js
+++ PAHK Website(Actual File)/framework/src/navibar.js
@@ -1,17 +1,28 @@
 import React from 'react';
-
+import { Route, NavLink, Routes } from 'react-router-dom'
 export default function Navibar(){
     return(
-        <nav>
-            <ul>
-                <a href=""><li>a</li></a>
-                <a href=""><li>a</li></a>
-                <a href=""><li>a</li></a>
-                <a href=""><li>a</li></a>
-                <a href=""><li>a</li></a>
-                <a href=""><li>a</li></a>
-            </ul>
-        </nav>
+        <div id='nav'>
+            <nav>
+                <ul>
+                    <NavLink exact to="/">Home</NavLink>
+                    <NavLink to="/aboutus">About Us</NavLink>
+                    <NavLink to="/pands">Products and Services</NavLink>
+                    <NavLink to="/contactus">Contact Us</NavLink>
+                    <NavLink to="/mediaroom">Media Room</NavLink>
+                    <NavLink to="/appendix">Appendix</NavLink>
+                </ul>
+            </nav>
+
+            <Routes>
+                <Route exact path="/"></Route>
+                <Route path="/aboutus"></Route>
+                <Route path="/pands"></Route>
+                <Route path="/contactus"></Route>
+                <Route path="/mediaroom"></Route>
+                <Route path="/appendix"></Route>
+            </Routes>
+        </div>
     )
 }
 
