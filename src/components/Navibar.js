3b1825ce97c2064833ae1f4d348a8b8bcb0d355d
Andy Poon <andypoon923@gmail.com>
2022-11-27 01:15:54 +0800

    Error in taskbar ActiveBar() function

--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -1,5 +1,6 @@
 import "./styles.css"
 import React from 'react';
+import { ActiveBar } from "./ActiveBar";
 import { Route, a, Routes } from 'react-router-dom';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 export default function Navibar(){
@@ -8,12 +9,12 @@ export default function Navibar(){
             <nav className='nav'>
                 <h1>Site Name</h1>
                 <ul>
-                    <li className="active"><a href="/">Home</a></li>
-                    <li><a href="/aboutus">About Us</a></li>
-                    <li><a href="/pands">Products and Services</a></li>
-                    <li><a href="/contactus">Contact Us</a></li>
-                    <li><a href="/mediaroom">Media Room</a></li>
-                    <li><a href="/appendix">Appendix</a></li>
+                    <li id = "HomeTab" onClick={ActiveBar()}><a href="/">Home</a></li>
+                    <li id = "AboutUsTab"><a href="/aboutus">About Us</a></li>
+                    <li id = "PandsTab"><a href="/pands">Products and Services</a></li>
+                    <li id = "ContactUsTab"><a href="/contactus">Contact Us</a></li>
+                    <li id = "MediaRoomTab"><a href="/mediaroom">Media Room</a></li>
+                    <li id = "AppendixTab"><a href="/appendix">Appendix</a></li>
                 </ul>
             </nav>
 
