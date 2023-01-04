94e027fbce6f7e0467a1e5910b55ec4bd7878ba5
Zeth Tang <zethtang@gmail.com>
2023-01-03 23:20:08 -0800

    fixed

--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -1,16 +1,18 @@
+import "./styles.css"
 import React from 'react';
-import { Route, NavLink, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 export default function Navibar(){
     return(
-        <div class='nav'>
-            <nav>
+        <div>
+            <nav className='nav'>
+                <h1>Site Name</h1>
                 <ul>
-                    <NavLink exact to="/">Home</NavLink>
-                    <NavLink to="/aboutus">About Us</NavLink>
-                    <NavLink to="/pands">Products and Services</NavLink>
-                    <NavLink to="/contactus">Contact Us</NavLink>
-                    <NavLink to="/mediaroom">Media Room</NavLink>
-                    <NavLink to="/appendix">Appendix</NavLink>
+                    <li id = "HomeTab"><a href="/">Home</a></li>
+                    <li id = "AboutUsTab"><a href="/aboutus">About Us</a></li>
+                    <li id = "PandsTab"><a href="/pands">Products and Services</a></li>
+                    <li id = "ContactUsTab"><a href="/contactus">Contact Us</a></li>
+                    <li id = "MediaRoomTab"><a href="/mediaroom">Media Room</a></li>
+                    <li id = "AppendixTab"><a href="/appendix">Appendix</a></li>
                 </ul>
             </nav>
 
@@ -22,7 +24,10 @@ export default function Navibar(){
                 <Route path="/mediaroom"></Route>
                 <Route path="/appendix"></Route>
             </Routes>
+           
+            
         </div>
     )
 }
+
 
